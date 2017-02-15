5943460ca45bbe5ead9ed9d7839f9e11d2401980
Dan <danbarry16@googlemail.com>
2017-02-15 11:21:34 +0000

    Implement ability to process comments

--- src/Post.java
+++ src/Post.java
@@ -1,6 +1,10 @@
 package barray.fc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Post
@@ -9,8 +13,23 @@ import java.io.File;
  **/
 public class Post implements Request{
   private static final int DEF_CMDS_MAX_LENGTH = 256;
+  private static final byte[] DEF_INDICATOR = "cmnt=".getBytes();
+  private static final byte[] DEF_HEADER =
+    "HTTP/1.1 200 OK\n\rContent-Type: text/html\n\r\n\r".getBytes();
+  private static final byte[] DEF_COMMENT_DATA =
+    ("<form action=\"?\" method=\"post\">" +
+      "<textarea name=\"cmnt\" style=\"width:100%;\">" +
+      "</textarea>" +
+      "<br>" +
+      "<input type=\"submit\" value=\"Submit\">" +
+      "</form>"
+    ).getBytes();
+  private static final byte[] DEF_FAILURE = "Error: Failed to post".getBytes();
 
+  private String ip;
+  private Get get;
   private File file;
+  private String cmnt;
 
   /**
    * Post()
@@ -23,13 +42,54 @@ public class Post implements Request{
    * @param ip The IP address of the client.
    **/
   public Post(File path, byte[] data, String ip){
+    this.ip = ip;
+    /* Build get request to return file */
+    get = new Get(path, data);
+    /* Process header */
     String[] cmds = new String(data, 0, DEF_CMDS_MAX_LENGTH).split(" ");
-    file = new File(path.getPath() + cmds[1]);
+    cmds[1] = cmds[1].replace("/", "");
+    cmds[1] = cmds[1].split("\\?")[0];
+    file = new File(path.getPath() + "/" + cmds[1]);
+    /* Get comment */
+    cmnt = null;
+    for(int x = data.length - 1 - DEF_INDICATOR.length; x >= 0; x--){
+      if(data[x] == '\n' || data[x] == '\r'){
+        cmnt = new String(
+          data,
+          x + DEF_INDICATOR.length + 1,
+          data.length - x - DEF_INDICATOR.length - 1
+        ).trim();
+        break;
+      }
+    }
+    /* TODO: Apply filtering here. */
   }
 
   public byte[] process(){
-    /* TODO: Implement this section. */
-    return "<h1>POST: I am alive!</h1>".getBytes();
+    /* Check whether the file exists */
+    if(!file.exists()){
+      if(!createCommentFile(file)){
+        Server.error("Post", "failed to create new file `" + file.getPath() + "`");
+        return DEF_FAILURE;
+      }
+    }
+    /* Check that the file is a valid comment file */
+    if(!Hash.check(file.getName())){
+      Server.error("Post", "invalid comments files `" + file.getPath() + "`");
+      return DEF_FAILURE;
+    }
+    /* Append comment to file */
+    try{
+      FileWriter fw = new FileWriter(file, true);
+      fw.write("\n<p><b>" + ip + " says: </b>" + cmnt + "</p>");
+      fw.flush();
+      fw.close();
+    }catch(IOException e){
+      Server.error("Post", "failed to append comment to `" + file.getPath() + "`");
+      return DEF_FAILURE;
+    }
+    /* Return the normal result of a GET request */
+    return get.process();
   }
 
   /**
